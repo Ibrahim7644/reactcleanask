8a8ce5f0225bc335089483d99700de8bce197a09
unknown <engr.ibrahim649@gmail.com>
2022-02-16 14:46:39 +0500

    set sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -7,37 +7,38 @@ import { VscRootFolder, VscInbox } from "react-icons/vsc";
 
 function Sidebar(props) {
     return (
-            <div class="row flex-nowrap">
+        <div>
+            <div class="row ">
                 <div class=" bg-dark">
-                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <div class="col-12 pl-3 align-items-stretch d-flex flex-column align-items-center align-items-sm-start text-white min-vh-100">
 
                         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                             <br></br>
                             <li class="nav-item">
                                 <a href="#" class="nav-link align-middle px-0">
-                                    <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline"> &emsp;<AiFillSignal size={30} /></span>
+                                    <i class="fs-4 bi-house"></i> <span class="ms-1  d-sm-inline "> &emsp;< AiFillSignal size={30} /></span>
                                 </a>
                             </li>
                             <br></br>
                             <li>
                                 <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                                    <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline"> &emsp;<RiLayoutGridLine size={30} /></span> </a>
+                                    <i class="fs-4 bi-speedometer2"></i> <span class="ms-1  d-sm-inline"> &emsp;<RiLayoutGridLine size={30} /></span> </a>
 
                             </li>
                             <br></br>
                             <li>
                                 <a href="#" class="nav-link px-0 align-middle">
-                                    <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline"> &emsp;<RiUser3Line size={30} /></span></a>
+                                    <i class="fs-4 bi-table"></i> <span class="ms-1  d-sm-inline"> &emsp;<RiUser3Line size={30} /></span></a>
                             </li>
                             <br></br>
                             <li>
                                 <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                                    <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">&emsp;<VscRootFolder size={30} /></span></a>
+                                    <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-sm-inline">&emsp;<VscRootFolder size={30} /></span></a>
                             </li>
                             <br></br>
                             <li>
                                 <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                                    <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">&emsp;<VscInbox size={30} /></span> </a>
+                                    <i class="fs-4 bi-grid"></i> <span class="ms-1  d-sm-inline">&emsp;<VscInbox size={30} /></span> </a>
                             </li>
                             <br></br>
 
@@ -46,7 +47,8 @@ function Sidebar(props) {
                 </div>
 
             </div>
-   
+        </div>
+
     )
 }
 
