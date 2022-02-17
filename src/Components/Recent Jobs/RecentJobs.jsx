7004390e50ef46081bd1cc645167c43ba654dd70
Kashif <kashifshah5898@gmail.com>
2022-02-17 18:11:36 +0500

    Past week inquiries component

--- src/Components/Recent Jobs/RecentJobs.jsx
+++ src/Components/Recent Jobs/RecentJobs.jsx
@@ -5,7 +5,7 @@ export default function RecentJobs() {
   return (
     <>
       <div className="col-lg-6 col-md-6 d-flex align-items-stretch col-sm-12 col-xs-12">
-        <div class="card recentJobsCard" style={{ borderRadius: "2%" }}>
+        <div class="card recentJobsCard">
           <div class="card-body">
             <h6 class="card-title"> Recent Jobs </h6>
             <p class="card-text">Chart2 here</p>
