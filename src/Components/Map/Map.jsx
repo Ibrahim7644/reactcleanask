424a54a23be268bc9a39e2aa18d95aa3a8bccda0
Kashif <kashifshah5898@gmail.com>
2022-02-21 11:27:45 +0500

    start working on Map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import "./Map.css";
+
 
 export default function Map() {
   return (
     <>
       <div class="card mapCard">
         <div class="card-body text-center">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            cumque voluptates maxime ipsa praesentium quasi ab accusamus eos,
-            veniam placeat ullam incidunt! Alias, facilis. Nobis consequuntur id
-            consequatur nihil fugit.
-          </p>
+          <p>Map HEre</p>
         </div>
       </div>
     </>
   );
 }
+
