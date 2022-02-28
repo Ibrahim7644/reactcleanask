937c58b33e197e8540f606621053bec2a0ce92b5
Kashif <kashifshah5898@gmail.com>
2022-02-28 11:42:58 +0500

    responsive sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,81 +6,69 @@ import { VscRootFolder, VscInbox } from "react-icons/vsc";
 
 function Sidebar(props) {
   return (
-    <div class="d-flex  align-items-center align-items-sm text-white">
-  
-        <ul
-          class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm"
-          id="menu"
-        >
-          <br></br>
-          <li class="nav-item">
-            <a href="#" class="nav-link align-middle px-0">
-              <i class="fs-4 bi-house"></i>{" "}
-              <span class="ms-1  d-sm-inline ">
-                {" "}
-                &emsp;
-                <AiFillSignal size={30} />
-              </span>
-            </a>
-          </li>
-          <br></br>
-          <li>
-            <a
-              href="#submenu1"
-              data-bs-toggle="collapse"
-              class="nav-link px-0 align-middle"
-            >
-              <i class="fs-4 bi-speedometer2"></i>{" "}
-              <span class="ms-1  d-sm-inline">
-                {" "}
-                &emsp;
-                <RiLayoutGridLine size={30} />
-              </span>{" "}
-            </a>
-          </li>
-          <br></br>
-          <li>
-            <a href="#" class="nav-link px-0 align-middle">
-              <i class="fs-4 bi-table"></i>{" "}
-              <span class="ms-1  d-sm-inline">
-                {" "}
-                &emsp;
-                <RiUser3Line size={30} />
-              </span>
-            </a>
-          </li>
-          <br></br>
-          <li>
-            <a
-              href="#submenu2"
-              data-bs-toggle="collapse"
-              class="nav-link px-0 align-middle "
-            >
-              <i class="fs-4 bi-bootstrap"></i>{" "}
-              <span class="ms-1 d-sm-inline">
-                &emsp;
-                <VscRootFolder size={30} />
-              </span>
-            </a>
-          </li>
-          <br></br>
-          <li>
-            <a
-              href="#submenu3"
-              data-bs-toggle="collapse"
-              class="nav-link px-0 align-middle"
-            >
-              <i class="fs-4 bi-grid"></i>{" "}
-              <span class="ms-1  d-sm-inline">
-                &emsp;
-                <VscInbox size={30} />
-              </span>{" "}
-            </a>
-          </li>
-          <br></br>
-        </ul>
-      </div>
-
+    <div class="text-white">
+      <ul class="nav nav-pills d-flex align-items-center flex-column" id="menu">
+        <br></br>
+        <li class="nav-item">
+          <a href="#" class="nav-link align-middle px-0">
+            <i class="fs-4 bi-house"></i>{" "}
+            <span class="ms-1  d-sm-inline ">
+              <AiFillSignal size={20} />
+            </span>
+          </a>
+        </li>
+        <br></br>
+        <li>
+          <a
+            href="#submenu1"
+            data-bs-toggle="collapse"
+            class="nav-link px-0 align-middle"
+          >
+            <i class="fs-4 bi-speedometer2"></i>{" "}
+            <span class="ms-1  d-sm-inline">
+              <RiLayoutGridLine size={20} />
+            </span>{" "}
+          </a>
+        </li>
+        <br></br>
+        <li>
+          <a href="#" class="nav-link px-0 align-middle">
+            <i class="fs-4 bi-table"></i>{" "}
+            <span class="ms-1  d-sm-inline">
+              {" "}
+              <RiUser3Line size={20} />
+            </span>
+          </a>
+        </li>
+        <br></br>
+        <li>
+          <a
+            href="#submenu2"
+            data-bs-toggle="collapse"
+            class="nav-link px-0 align-middle "
+          >
+            <i class="fs-4 bi-bootstrap"></i>{" "}
+            <span class="ms-1 d-sm-inline">
+              <VscRootFolder size={20} />
+            </span>
+          </a>
+        </li>
+        <br></br>
+        <li>
+          <a
+            href="#submenu3"
+            data-bs-toggle="collapse"
+            class="nav-link px-0 align-middle"
+          >
+            <i class="fs-4 bi-grid"></i>{" "}
+            <span class="ms-1  d-sm-inline">
+              <VscInbox size={20} />
+            </span>{" "}
+          </a>
+        </li>
+        <br></br>
+      </ul>
+    </div>
   );
 }
 
--- src/Pages/Dashboard/Main.jsx
+++ src/Pages/Dashboard/Main.jsx
@@ -3,7 +3,7 @@ import Nav from "../../Components/Navbar/Nav";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Footer from "../../Components/Footer/Footer";
 import Kpi from "../../Components/Kpi/Kpi";
-import Login from '../../Pages/Login/Login';
+import Login from "../../Pages/Login/Login";
 
 import "./Main.css";
 import PastWeekInquiries from "../../Components/Past week inquiries/PastWeekInquiries";
@@ -23,93 +23,87 @@ import { BsFillPersonBadgeFill } from "react-icons/bs";
 
 function Main(props) {
   return (
-
-      <div className="main_menu">
-        <div className="row">
-          <div className="col-12">
-            <Nav />
-          </div>
+    <div className="main_menu">
+      <div className="row">
+        <div className="col-12">
+          <Nav />
         </div>
-        <div className="row ">
-          <div style={{border : "2px solid red"}} className="sidebar col-1 bg-dark">
-           
-   
-                <Sidebar />
-       
-           
-          </div>
-          {/* ROW 1 */}
-          <div className="col-11">
-            <div className="row">
-              <div class="row mt-2 cardsCenter">
-                <Kpi
-                  title="Search Vendors"
-                  icon={<MdPersonSearch size={"40"} color={"orange"} />}
-                />
-                <Kpi
-                  title="Manage Contracts"
-                  icon={<GiNewspaper size={"40"} color={"orange"} />}
-                />
-                <Kpi
-                  title="Order Supplies"
-                  icon={<FiTruck size={"40"} color={"orange"} />}
-                />
-                <Kpi
-                  title="Manage Finances"
-                  icon={<GiTakeMyMoney size={"40"} color={"orange"} />}
-                />
-                <Kpi
-                  title="Manage Clients"
-                  icon={<BsFillPersonBadgeFill size={"40"} color={"orange"} />}
-                />
-              </div>
+      </div>
+      <div className="row ">
+        <div className="col-1 bg-dark">
+          <Sidebar />
+        </div>
+        {/* ROW 1 */}
+        <div className="col-11">
+          <div className="row">
+            <div class="row mt-2 cardsCenter">
+              <Kpi
+                title="Search Vendors"
+                icon={<MdPersonSearch size={"40"} color={"orange"} />}
+              />
+              <Kpi
+                title="Manage Contracts"
+                icon={<GiNewspaper size={"40"} color={"orange"} />}
+              />
+              <Kpi
+                title="Order Supplies"
+                icon={<FiTruck size={"40"} color={"orange"} />}
+              />
+              <Kpi
+                title="Manage Finances"
+                icon={<GiTakeMyMoney size={"40"} color={"orange"} />}
+              />
+              <Kpi
+                title="Manage Clients"
+                icon={<BsFillPersonBadgeFill size={"40"} color={"orange"} />}
+              />
+            </div>
 
-              {/* ROW 2 */}
-              <div class="row mt-2">
-                <PastWeekInquiries />
-                <RecentJobs />
-                <Progress />
-              </div>
+            {/* ROW 2 */}
+            <div class="row mt-2">
+              <PastWeekInquiries />
+              <RecentJobs />
+              <Progress />
+            </div>
 
-              {/* ROW 3 */}
-              <div class="row mt-2">
-                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 ">
-                  <div class="col-lg-12 col-md-12  col-sm-12 col-xs-12 min-vh-50">
-                    <Notifications />
-                  </div>
-                </div>
-                <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12 ">
-                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <IncomingInquiries />
-                  </div>
-                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-                    <ActiveJobs />
-                  </div>
+            {/* ROW 3 */}
+            <div class="row mt-2">
+              <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 ">
+                <div class="col-lg-12 col-md-12  col-sm-12 col-xs-12 min-vh-50">
+                  <Notifications />
                 </div>
               </div>
+              <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12 ">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <IncomingInquiries />
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                  <ActiveJobs />
+                </div>
+              </div>
+            </div>
 
-              {/* ROW 4 */}
-              <div class="row mt-2">
-                <div className="col-lg-4 col-md-4 align-items-stretch col-sm-12 col-xs-12">
-                  <Map />
-                </div>
-                <div className="col-lg-8 col-md-8  align-items-stretch col-sm-12 col-xs-12">
-                  <ScheduledJobs />
-                </div>
+            {/* ROW 4 */}
+            <div class="row mt-2">
+              <div className="col-lg-4 col-md-4 align-items-stretch col-sm-12 col-xs-12">
+                <Map />
               </div>
-              <div>
-                <br />
+              <div className="col-lg-8 col-md-8  align-items-stretch col-sm-12 col-xs-12">
+                <ScheduledJobs />
               </div>
+            </div>
+            <div>
+              <br />
             </div>
           </div>
         </div>
-        <div className="row ">
-          <div className="col-12">
-            <Footer />
-          </div>
+      </div>
+      <div className="row ">
+        <div className="col-12">
+          <Footer />
         </div>
       </div>
- 
+    </div>
   );
 }
 
